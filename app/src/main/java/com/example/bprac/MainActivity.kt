a03c25d793d19becb677d57d3463af9d9086f910
Sumner Lewis <lewiss33@wwu.edu>
2023-01-23 23:46:26 -0800

    added playback functionality to change channel button

--- app/src/main/java/com/example/bprac/MainActivity.kt
+++ app/src/main/java/com/example/bprac/MainActivity.kt
@@ -1,17 +1,25 @@
+//note: this version uses the toggle button to stop audio recording, and the change channel button to play it back.
+// app will crash when record is pushed again. Went back to older (just recording) version and bug existed there too, odd I didn't catch it before.
+// eventually the bytes will be sent out and not saved locally so perhaps this bug doesn't need to be fixed
+// if you comment out the release() of mediarecorder in the stopRecording, it will eliminate the crash, but the recording functionality will still be broken post-stop
+
 package com.example.bprac
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private var mediaRecorder: MediaRecorder? = null
     private var state: Boolean = false
     private var recordingStopped: Boolean = false
+    var mMediaPlayer: MediaPlayer? = null //private?
 
     @SuppressLint("MissingInflatedId") //cant remember what this is
     @Override //not sure why i needed this either
@@ -35,6 +44,27 @@ class MainActivity : AppCompatActivity() {
         mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4) //correct format?
         mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)    //correct encoder?
         mediaRecorder?.setOutputFile(output)
+
+        fun playRecording(uri: Uri) {
+            var mMediaPlayer: MediaPlayer? = null
+            try {
+                mMediaPlayer = MediaPlayer().apply {
+                    setDataSource(application, uri)
+                    setAudioAttributes(
+                        AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build()
+                    )
+                    prepare()
+                    start()
+                }
+            } catch (exception: IOException) {
+                mMediaPlayer?.release()
+                mMediaPlayer = null
+            }
+        }
+
 
         fun startRecording() {
             try {
@@ -82,8 +112,11 @@ class MainActivity : AppCompatActivity() {
 
         val changeChannel = findViewById<Button>(R.id.change_channel)
         changeChannel.setOnClickListener {
-            val intent = Intent(this,AppStart::class.java)
-            startActivity(intent)
+            //val intent = Intent(this,AppStart::class.java)   //these two lines are the correct function, but temporarily using this as the playback button
+            // startActivity(intent)
+            val file = File(Environment.getExternalStorageDirectory(), "recording.mp3")
+            val uri = Uri.fromFile(file)
+            playRecording(uri)
         }
 
     }
