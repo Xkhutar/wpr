ec148554888c1e557c2882afb04f33db6bd87635
Sumner Lewis <lewiss33@wwu.edu>
2023-02-27 16:59:45 -0800

    I didn't nuke main. Never happened.

--- app/src/main/java/com/example/bprac/MainActivity.kt
+++ app/src/main/java/com/example/bprac/MainActivity.kt
@@ -4,12 +4,16 @@ package com.example.bprac
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.ContextWrapper
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.net.Uri
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -20,14 +24,154 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.File
 import java.io.IOException
+import android.net.wifi.p2p.WifiP2pConfig
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pGroup
+import android.net.wifi.p2p.WifiP2pManager.*
+import android.provider.Settings
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+import java.util.*
+
+class MainActivity : AppCompatActivity(), ChannelListener {
 
     private var output: String? = null
     private var mediaRecorder: MediaRecorder? = null
     private var state: Boolean = false
     private var recordingStopped: Boolean = false
     var mMediaPlayer: MediaPlayer? = null //private?
+
+    private var manager: WifiP2pManager? = null
+    private var isWifiP2pEnabled = false
+    private var retryChannel = false
+
+    private val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
+    private var channel: WifiP2pManager.Channel? = null
+    private var receiver: BroadcastReceiver? = null
+
+    fun setIsWifiP2pEnabled(isWifiP2pEnabled: Boolean) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled
+    }
+
+
+    public override fun onResume() {
+        super.onResume()
+        receiver?.also { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        receiver?.also { receiver ->
+            unregisterReceiver(receiver)
+        }
+    }
+
+    override fun connect(config: WifiP2pConfig) {
+        manager!!.connect(channel, config, object : ActionListener {
+
+            override fun onSuccess() {
+                // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
+            }
+
+            override fun onFailure(reason: Int) {
+                Toast.makeText(this@WiFiDirectActivity, "Connect failed. Retry.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun disconnect() {
+        val fragment = supportFragmentManager
+            .findFragmentById(R.id.frag_detail) as DeviceDetailFragment
+        fragment.resetViews()
+        manager!!.removeGroup(channel, object : ActionListener {
+
+            override fun onFailure(reasonCode: Int) {
+                Log.d(TAG, "Disconnect failed. Reason :$reasonCode")
+
+            }
+
+            override fun onSuccess() {
+                fragment.view!!.visibility = View.GONE
+            }
+
+        })
+    }
+
+    override fun onChannelDisconnected() {
+        // we will try once more
+        if (manager != null && !retryChannel) {
+            Toast.makeText(this, "Channel lost. Trying again", Toast.LENGTH_LONG).show()
+            resetData()
+            retryChannel = true
+            manager!!.initialize(this, mainLooper, this)
+        } else {
+            Toast.makeText(this,
+                "Severe! Channel is probably lost premanently. Try Disable/Re-Enable P2P.",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun cancelDisconnect() {
+
+        /*
+         * A cancel abort request by user. Disconnect i.e. removeGroup if
+         * already connected. Else, request WifiP2pManager to abort the ongoing
+         * request
+         */
+        if (manager != null) {
+            val fragment = supportFragmentManager
+                .findFragmentById(R.id.frag_list) as DeviceListFragment
+            if (fragment.device == null || fragment.device!!.status == WifiP2pDevice.CONNECTED) {
+                disconnect()
+            } else if (fragment.device!!.status == WifiP2pDevice.AVAILABLE || fragment.device!!.status == WifiP2pDevice.INVITED) {
+
+                manager!!.cancelConnect(channel, object : ActionListener {
+
+                    override fun onSuccess() {
+                        Toast.makeText(this@WiFiDirectActivity, "Aborting connection",
+                            Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onFailure(reasonCode: Int) {
+                        Toast.makeText(this@WiFiDirectActivity,
+                            "Connect abort request failed. Reason Code: $reasonCode",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }
+    }
+
+
+    private fun createGroup() {
+        manager?.also { manager ->
+
+            manager.requestGroupInfo(channel) { group ->
+                Log.d(TAG, "createGroup group:$group")
+            }
+
+            manager.createGroup(channel, object : ActionListener {
+                override fun onSuccess() {
+                    Toast.makeText(this@WiFiDirectActivity, "create group success", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(reason: Int) {
+                    Toast.makeText(this@WiFiDirectActivity, "create group failure", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+    }
 
     @SuppressLint("MissingInflatedId") //cant remember what this is
     @Override //not sure why i needed this either
@@ -101,6 +245,12 @@ class MainActivity : AppCompatActivity() {
                 mMediaPlayer?.release()
                 mMediaPlayer = null
             }
+
+            manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+            channel = manager?.initialize(this, mainLooper, null)
+            channel?.also { channel ->
+                receiver = WiFiDirectBroadcastReceiver(manager, channel, this)
+            }
         }
 
 
@@ -163,6 +313,10 @@ class MainActivity : AppCompatActivity() {
             playRecording(uri)
         }
 
+    }
+
+    companion object {
+        private const val TAG = "wpr"
     }
 
 }
